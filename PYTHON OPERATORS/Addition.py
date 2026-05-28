9ec9e23a0e607bf9165b1d3c8540dc80fcea0f28
PratikThorat-195 <pratikthorat195@gmail.com>
2026-05-28 15:53:36 +0530

    code on operators

---
+++ PYTHON OPERATORS/Addition.py
@@ -0,0 +1,6 @@
+# Addition for two numbers
+print("Addition Operation for testing Addition Operator in Python")
+num1 = int(input("Enter first value : "))
+num2 = int(input("Enter Second Number: "))
+sum = num1 + num2
+print(sum)
---
+++ PYTHON OPERATORS/AverageOf2num.py
@@ -0,0 +1,5 @@
+print("Average of two given number")
+num1 = int(input("Enter a 1st number: "))
+num2 = int(input("Enter a 2nd number: "))
+avg = num1 + num2 / 2
+print("AVERAGE is", avg)
