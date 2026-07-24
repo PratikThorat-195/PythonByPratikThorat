8d90e54b5bd359a0216ac16a39d18c171b23c7f5
PratikThorat-195 <pratikthorat195@gmail.com>
2026-07-24 12:25:50 +0530

    STR practice code

---
+++ PYTHON_DATA_TYPES/Fundamental_Data_Types/COMPLEX.py
@@ -0,0 +1,29 @@
+# ========================================================================
+#                   Complex
+# ========================================================================
+# Properties
+# 'complex' is one of the pre-defined class and treated as Fundamental data type.
+# The purpose of complex data type is that 'To Store complex value / Imaginary values'
+
+# examples
+a = 2+3j 
+print(a,type(a)) #------------- (2+3j) <class 'complex'>
+
+a = -2-4j
+print(a, type(a)) #------------- (-2-4j) <class 'complex'>
+
+b = 1.2+3.4j
+print(b, type(b)) #------------- (1.2+3.4j) <class 'complex'>
+
+b = 12+4.5J
+print(b, type(b)) #------------- (12+4.5j) <class 'complex'>
+
+c = 5j
+print(c, type(c)) #------------- 5j <class 'complex'>
+
+c = -5j
+print(c, type(c)) #------------- (-0-5j) <class 'complex'>
+
+d = -2.6j
+print(d, type(d)) #------------- (-0-2.6j) <class 'complex'>
+
---
+++ PYTHON_DATA_TYPES/Fundamental_Data_Types/STR.py
@@ -0,0 +1,65 @@
+s1 = "Python"
+print(s1, type(s1));
+
+s2 = "Python Programming"
+print(s2, type(s2))
+
+s3 = "P"
+print(s3,type(s3))
+
+s4 = 'p'
+print(s4, type(s4))
+
+s5 = '''add any text 
+pewroigjwporigj i4ughwrpuigj ;oigj
+foqieuwrfhoiueh 
+here''' 
+print(s5, type(s5))
+
+s6 = """lasdfjlaiecne
+qekdfhcgoduvhoieufvh
+qekljvbqlekjfvn"""
+print(s6, type(s6))
+
+s7 = '!@#$%^&*()'
+print(s7, type(s7))
+
+s8 = """ Guido van Rossum
+... w0regilfjvhorfvuihwrf
+... qeikruhwoeruhower9fuhwoer
+... qoieurvhw oqeirufhqoe oqierufh"""
+print(s8, type(s8))
+
+s9 = """ 12344357 \n 652345356246 \n 967896789678967 """
+print(s9, type(s9))
+
+a = "PYTHON"
+print(a[0])
+print(a[-1])
+print(a[-2])
+print(a[5])
+# print(a[6])
+# print(a[-7])
+s4 = "123456789"
+s4[len(s4)-1]
+print(len(s4)) 
+print(s4[len(s4)-1])
+
+a = "PYTHON"
+a[2:1]
+print(a[2:1])
+print(a[0:3])
+print(a[2:6])
+print(a[1:7])
+print(a[4:5])
+
+# ===========================================================
+print("--------------------------------")
+b = "PYTHON"
+print(b[0:])
+print(b[2:])
+print(b[5:])
+print(b[6:])
+
+# special case ===========================================================
+print(b[-66:-1]) 
